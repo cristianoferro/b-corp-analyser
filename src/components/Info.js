2ef1324d0175706898bb0e765494881432b3ae5b
Cristiano Ferro <cristianobferro@gmail.com>
2021-02-18 17:44:09 +0000

    improved animations with install of framer motion

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,32 +1,44 @@
 import { useState, useRef }from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGlobeEurope, faSignal, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 
 const Info = ({ currentCorp }) => {
     const [aboutClicked, setAboutClicked] = useState(false)
     const about = useRef(null);
+
     const aboutClickHandler = (event) => {
         setAboutClicked(!aboutClicked);
-        // about.current.style={bottom:`30vw`};
     }
 
     return (
-        <div className={`info ${aboutClicked?'info-scroll':''}`}>
+        <motion.div className="info"
+            animate={{y:`${aboutClicked?'-58.5vh':'0vh'}`}} 
+            transition={{duration:0.5, ease:'easeInOut'}}
+        >
             
-            <div className="corp-title">
+            <motion.div className="corp-title"
+            animate={{opacity:`${aboutClicked?'0':'1'}`}}
+            >
                 <h1>{currentCorp.currentCorp.company}</h1>
-            </div>
+            </motion.div>
                 
-            <div className="info-rank">
+            <motion.div className="info-rank"
+            animate={{opacity:`${aboutClicked?'0':'1'}`}}
+            >
                 <FontAwesomeIcon icon={faSignal} />
                 <span>{currentCorp.currentCorp.category_rank} </span>
                 <FontAwesomeIcon icon={faGlobeEurope} />
                 <span>{currentCorp.currentCorp.global_rank}</span>
-            </div>
-            <div className="logo">
+            </motion.div>
+            <motion.div className="logo"
+            animate={{opacity:`${aboutClicked?'0':'1'}`}}
+            >
                 <img src={currentCorp.currentCorp.logo} alt="Category Ranking" />
-            </div>
-            <p className="description">{currentCorp.currentCorp.description}</p>
+            </motion.div>
+            <motion.p className="description"
+            animate={{opacity:`${aboutClicked?'0':'1'}`}}
+            >{currentCorp.currentCorp.description}</motion.p>
 
             <button onClick={aboutClickHandler} className="about-btn">
                 <p> 
@@ -34,11 +46,14 @@ const Info = ({ currentCorp }) => {
                 </p> 
                 <FontAwesomeIcon icon={faChevronDown}/>
             </button>
-            <img className="down-arrow" src="" alt="" />
 
-            <p className={`about ${aboutClicked?'about-show':''}`}>Ocean Bottle enables anyone who gives a damn about our Ocean the opportunity to turn the tap off Ocean Plastic. Each bottle is smart chip activated and funds collection of ocean-bound plastic, whilst setting up recycling infrastructure in coastal communities with high levels of plastic pollution around the world. Plastic collectors are able to exchange plastic waste for money or digital tokens via blockchain technology to spend on tuition, tech goods, health care and micro-finance. To date, we've funded the collection of over 1 Million kilograms of plastic from entering the Ocean.</p>
+            <motion.p className="about"
+                animate={{opacity:`${aboutClicked?'1':'0'}`}}
+            >
+                Ocean Bottle enables anyone who gives a damn about our Ocean the opportunity to turn the tap off Ocean Plastic. Each bottle is smart chip activated and funds collection of ocean-bound plastic, whilst setting up recycling infrastructure in coastal communities with high levels of plastic pollution around the world. Plastic collectors are able to exchange plastic waste for money or digital tokens via blockchain technology to spend on tuition, tech goods, health care and micro-finance. To date, we've funded the collection of over 1 Million kilograms of plastic from entering the Ocean.
+            </motion.p>
 
-        </div>
+        </motion.div>
     );
 }
 
