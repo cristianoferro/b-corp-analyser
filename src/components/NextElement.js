ce18f902b61182132b243e487ab7f65d386eff9a
Cristiano Ferro <cristianobferro@gmail.com>
2021-02-18 17:42:23 +0000

    added nextElement component

--- src/components/NextElement.js
+++ src/components/NextElement.js
@@ -1,8 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobeEurope, faSignal, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 const NextElement = () => {
     return (
         <div className="next-element">
-            
+            <div className="side-rankings">
+                <p className="side-rank-title">Best in Category</p>
+                <div className="info-rank">
+                    <FontAwesomeIcon icon={faSignal} />
+                    <span>1</span>
+                    <FontAwesomeIcon icon={faGlobeEurope} />
+                    <span>15</span>
+                </div>
+            </div>
+            <div className="mini-badges">
+
+            </div>
         </div>
+        
 
     );
 }
